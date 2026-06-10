96c1018d8c8c9ebd38ecd17d3b05993164c6d9bf
logwet <logwet@users.noreply.github.com>
2026-06-10 21:00:50 +1000

    Initial commit

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE = 'opportunity-cost-v1';
+
+const PRECACHE = [
+    './',
+    './index.html',
+    './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(PRECACHE))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(cached => {
+            if (cached) {
+                return cached;
+            }
+
+            return fetch(event.request).then(response => {
+                if (
+                    response.ok &&
+                    event.request.url.startsWith(self.location.origin)
+                ) {
+                    const copy = response.clone();
+                    caches.open(CACHE).then(cache =>
+                        cache.put(event.request, copy)
+                    );
+                }
+
+                return response;
+            });
+        })
+    );
+});
