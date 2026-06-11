c28782bb3f582f267f6744a3e80e445d7b49bbcd
logwet <logwet@users.noreply.github.com>
2026-06-11 22:37:56 +1000

    Auto update + SEO

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'opportunity-cost-v6';
+const CACHE = 'opportunity-cost-v7';
 
 const PRECACHE = [
     './',
@@ -13,7 +13,12 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE).then(cache => cache.addAll(PRECACHE))
     );
-    self.skipWaiting();
+});
+
+self.addEventListener('message', event => {
+    if (event.data?.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
 });
 
 self.addEventListener('activate', event => {
